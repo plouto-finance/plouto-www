03394e7198d7400a7681c0529a44ccf0f5894a0a
developer <developer@plouto.finance>
2020-09-26 21:21:44 +0800

    fix rewards

--- src/actions/reward.ts
+++ src/actions/reward.ts
@@ -70,6 +70,10 @@ const getApy = async (web3: Web3, asset: any, address: string, callback: any) =>
     tokenPrice = 46357066.169
   } else if(asset.erc20address === '0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852') {
     tokenPrice = 46330982.88444
+  } else if(asset.erc20address === '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48') {
+    tokenPrice = 10 ** 12
+  } else if(asset.erc20address === '0xdAC17F958D2ee523a2206206994597C13D831ec7') {
+    tokenPrice = 10 ** 12
   }
 
   try {
